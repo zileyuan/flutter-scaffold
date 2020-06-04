5b9114cec932f2c046895e88afb8ec626ae130db
zileyuan <zileyuan@qq.com>
2020-06-04 09:16:03 +0800

    fix web & http

--- lib/routers/route_handlers.dart
+++ lib/routers/route_handlers.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:app/pages/index_page.dart';
 import 'package:app/pages/splash_page.dart';
 import 'package:app/pages/account/login_page.dart';
+import 'package:app/pages/account/private_page.dart';
 
 var splashPageHandler = new Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) =>
@@ -15,3 +16,7 @@ var indexPageHandler = new Handler(
 var loginPageHandler = new Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) =>
         LoginPage());
+
+var privatePageHandler = new Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) =>
+        PrivatePage());
